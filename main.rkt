9a112304fae1a9f7fcb335905ceca3e493a6fc63
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2018-11-14 16:58:09 -0800

    Added rudimentary parser

--- main.rkt
+++ main.rkt
@@ -26,15 +26,34 @@
   [l-nand (lhs Expr?) (rhs Expr?)]
   [l-nor (lhs Expr?) (rhs Expr?)]
   [l-xnor (lhs Expr?) (rhs Expr?)]
-  [l-not (exp Expr?)])
+  [l-not (nexp Expr?)])
 
 (define *reserved-symbols* '(id AND OR NOT XOR NAND NOR XNOR))
 
 ;; valid-identifer? : any -> boolean
-;; Determines whether the parameter is valis as an identifier name
+;; Determines whether the parameter is valid as an identifier name
 ;; (rejects any names that has been used in define-type)
 (define (valid-identifier? sym)
   (and (symbol? sym)
        (not (member sym *reserved-symbols*))))
 
+;; valid-boolean? : number -> number
+;; Determines whether the parameter is a valid b-value
+;; (only accepts the numbers 0 and 1)
+(define (valid-boolean? bool)
+  (and (number? bool)
+       (or (= bool 0) (= bool 1))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PARSING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (parse sexp)
+  (match sexp
+    [(? valid-identifier?) (id sexp)]
+    [(? valid-boolean?) (bvalue sexp)]
+    [(list lhs 'AND rhs) (l-and (parse lhs) (parse rhs))]
+    [(list lhs 'OR rhs) (l-or (parse lhs) (parse rhs))]
+    [(list lhs 'XOR rhs) (l-xor (parse lhs) (parse rhs))]
+    [(list lhs 'NAND rhs) (l-nand (parse lhs) (parse rhs))]
+    [(list lhs 'NOR rhs) (l-nor (parse lhs) (parse rhs))]
+    [(list lhs 'XNOR rhs) (l-xnor (parse lhs) (parse rhs))]
+    [(list 'NOT nexp) (l-not nexp)]
+    [else (error "Invalid syntax!")]))
